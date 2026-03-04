0f3b6b021d315ea8edf759149f3a997f7961b7b7
Fernando Asato <fp.asato.dev@gmail.com>
2026-03-03 21:52:57 -0300

    atividade

--- exercicio_dicionario.py
+++ exercicio_dicionario.py
@@ -1,11 +1,22 @@
-produto = {
-    "nome": "Mouse Gamer",
-    "preco": 150,
-    "estoque": 20,
-    "categoria": "Periféricos"
-}
+# produto = {
+#     "nome": "Mouse Gamer",
+#     "preco": 150,
+#     "estoque": 20,
+#     "categoria": "Periféricos"
+# }
+
+produto = {}
+
+produto['nome'] = input('Qual o nome do produto? ')
+produto["preco"]=float(input('Qual o preço do produto? '))
+produto["estoque"]= int(input('Qual a quantidade de produtos? '))
+produto["categoria"]= input('Qual a categoria do produto? ')
+
+
+# for chave, valor in produto.items():
+#     print(f'{chave}: {valor}') 
 
 print(produto)
-
-for chave, valor in produto.items():
-    print(f'{chave}: {valor}') 
+print()
+print(f'Produto: {produto["nome"]}')
+print(f"Preço: R${produto["preco"]}")
